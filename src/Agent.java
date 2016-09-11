3412b71559ea4db7cb0885efbb6394caea152fbb
jestter379 <Fabian.07.03.92@gmail.com>
2016-09-11 01:17:30 -0300

    arreglado

--- src/Agent.java
+++ src/Agent.java
@@ -1,32 +1,86 @@
+import java.io.*;
 
 public class Agent
 {
-
-	static final int BOARDSIZE=8;	/// The size of the board
 
 	private String filePath;
 	private int waitTime;
 
 	private Board iniBoard;
-	private int count;
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		new Agent(args[0],Integer.parseInt(args[1]));
 	}
 
-	public Agent(String fPath, int time)
+	public Agent(String fPath, int time) throws IOException
 	{
 		this.filePath = fPath;
 		this.waitTime = time;
 		this.iniBoard = new Board();
-		this.count = 0;
 
-		int[][] board = new int[BOARDSIZE][BOARDSIZE];
+
+		int[][] board = new int[Board.BOARDSIZE][Board.BOARDSIZE];
 		BufferedReader bf = new BufferedReader(new FileReader(fPath));
-		for(int i = 0;((String line = bf.readLine())!=null);i++)
+		int l = 0;
+		String line;
+		while((line = bf.readLine())!=null)
 		{
-			
+			if(l < 8)
+			{
+				String splited[] = line.split(" ");
+				for (int j=0; j < 8; j++)
+				{
+					board[l][j] = Integer.parseInt(splited[j]);
+					System.out.print(board[l][j]+" ");
+				}
+				System.out.println();
+			}
+			if(l == 8)
+			{
+				if(line.equals("B"))
+				{
+					iniBoard.setTurn(Board.TURNWHITE);
+				}
+				else
+				{
+					iniBoard.setTurn(Board.TURNBLACK);
+				}
+			}
+			else
+			{
+				if(line.startsWith("MovsHastaEmpate"))
+				{
+					iniBoard.setMovesToDraw(Integer.parseInt(line.split(" ")[1]));
+				}
+
+				else if(line.startsWith("EnroqueL_B"))
+				{
+					iniBoard.setLongCastle(iniBoard.turn , true);
+				}
+
+				else if(line.startsWith("EnroqueL_N"))
+				{
+					iniBoard.setLongCastle(iniBoard.turn , true);
+				}
+
+				else if(line.startsWith("EnroqueC_B"))
+				{
+					iniBoard.setShortCastle(iniBoard.turn , true);
+				}
+
+				else if(line.startsWith("EnroqueC_N"))
+				{
+					iniBoard.setShortCastle(iniBoard.turn , true);
+				}
+
+				else if(line.startsWith("AlPaso"))
+				{
+					iniBoard.setEnPassent(new Coord(line.split(" ")[1]));
+				}
+			}
+
+			l++;
 		}
 
 		iniBoard.fromArray(board);
@@ -40,15 +94,7 @@ public class Agent
 		{
 			Board aux = board.clone();
 			aux.makeMove(m);
-			if(!aux.isCheckMate())
-			{
-				count++;
-				DFS(aux);
-			}
-			else
-			{
-				return; //puede que aquí tenga que ir otra cosa xDDD
-			}
+			DFS(aux);
 		}
 
 	}
