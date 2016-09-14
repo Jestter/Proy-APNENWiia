e3e2d354ddff9775c797f5f61e4a911df0eccdcf
jestter379 <Fabian.07.03.92@gmail.com>
2016-09-13 21:47:37 -0300

    ids ready

--- src/Agent.java
+++ src/Agent.java
@@ -11,7 +11,14 @@ public class Agent
 
 	public static void main(String[] args) throws IOException
 	{
-		new Agent(args[0],Integer.parseInt(args[1]));
+		try
+		{
+			new Agent(args[0],Integer.parseInt(args[1]));
+		}
+		catch(IOException exc)
+		{
+			System.out.println("Error al leer archivo: "+args[0]);
+		}
 	}
 
 	public Agent(String fPath, int time) throws IOException
@@ -21,8 +28,17 @@ public class Agent
 		this.iniBoard = new Board();
 		this.count = 0;
 
+		loadTBL();
+		
+		long tini = System.nanoTime();
+		System.out.println("next move : "+ nextMove().toString()+"\nElapsed time: " + 
+			((System.nanoTime()-tini)/1e9) + " s");
+	}
+
+	public void loadTBL() throws IOException
+	{
 		int[][] board = new int[Board.BOARDSIZE][Board.BOARDSIZE];
-		BufferedReader bf = new BufferedReader(new FileReader(fPath));
+		BufferedReader bf = new BufferedReader(new FileReader(this.filePath));
 		int l = 0; //contador de lineas
 		String line;
 		while((line = bf.readLine())!=null)
@@ -83,74 +99,94 @@ public class Agent
 		}
 
 		iniBoard.fromArray(board);
-		System.out.println("next move : "+ nextMove().toString());
 	}
 
 	public Move nextMove()
 	{
 		Move bestMove = null;
-		long bestScoreSoFar = 0;
+		int bestScoreSoFar = -2;
 		Move[] validMoves = iniBoard.getValidMoves();
 		long depth = 0;
-		while(bestMove == null)
+		while(bestScoreSoFar != 1)
 		{
-			System.out.println("depth: "+depth);
+			//System.out.println("depth: "+depth);
 			for(Move m : validMoves)
 			{
 				Board aux = iniBoard.clone();
 				aux.makeMove(m);
 				count++;
 				//long score = DFS(aux);
-				long score = IDS(aux,depth);
-				if(score > 0)System.out.println("candidate : " + m.toString());
+				int score = IDS(aux,depth);
 				if(score > bestScoreSoFar) 
 				{
 					bestMove = m;
-					System.out.println("MoveChanged to " + m.toString());
+					//System.out.println("MoveChanged to " + m.toString());
 					bestScoreSoFar = score;
+					if(bestScoreSoFar == 1)break;
 				}
 			}
+			//System.out.println("mejor encontrado: "+bestScoreSoFar);
 			depth++;
 		}
 		return bestMove;
 	}
 
-	public long DFS(Board board)
+	public int DFS(Board board)
 	{
+		if(board.isCheckMate()) return (board.turn == iniBoard.turn? -1 : 1);
 		if(board.isStalemate()) return 0;
-		if(board.isCheckMate()) return (board.turn == iniBoard.turn? 0 : 1);
 
 		Move[] validMoves = board.getValidMoves();
-		long sum = 0;
+		int bestVal = (board.turn == iniBoard.turn? -1 : 1);
+
 		for(Move m : validMoves)
 		{
 			Board aux = board.clone();
 			aux.makeMove(m);
 			count++;
-			long score = DFS(aux);
-			sum += score;
+			int score = DFS(aux);
+			if(board.turn == iniBoard.turn)
+			{
+				bestVal = Math.max(bestVal, score);
+				if(bestVal == 1) return 1;
+			}
+			else
+			{
+				bestVal = Math.min(bestVal, score);
+				if(bestVal == -1) return -1;
+			}
 		}
 
-		return sum;
+		return bestVal;
 	}
 
-	public long IDS(Board board, long depth)
+	public int IDS(Board board, long depth)
 	{
-		if(board.isCheckMate()) return (board.turn == iniBoard.turn? 0 : 1);
+		if(board.isCheckMate()) return (board.turn == iniBoard.turn? -1 : 1);
 		if(board.isStalemate() || depth == 0) return 0;
 
 		Move[] validMoves = board.getValidMoves();
-		long sum = 0;
+		int bestVal = (board.turn == iniBoard.turn? -1 : 1);
+
 		for(Move m : validMoves)
 		{
 			Board aux = board.clone();
 			aux.makeMove(m);
 			count++;
-			long score = IDS(aux, depth-1);
-			sum += score;
+			int score = IDS(aux, depth-1);
+			if(board.turn == iniBoard.turn)
+			{
+				bestVal = Math.max(bestVal, score);
+				if(bestVal == 1) return 1;
+			}
+			else
+			{
+				bestVal = Math.min(bestVal, score);
+				if(bestVal == -1) return -1;
+			}
 		}
 
-		return sum;
+		return bestVal;
 	}
 
 }
